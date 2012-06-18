b8e2ce10562e6407c3cc7bd143622c733c0cf6b8
Mathieu Lecarme <mathieu@garambrogne.net>
2012-06-18 23:09:22 +0200

    Number of workers.

--- searchtags.go
+++ searchtags.go
@@ -3,11 +3,10 @@ package main
 var searchQueue chan queryAnswer
 
 func main() {
-	searchQueue = make(chan queryAnswer, 40)
-	go StartSearch()
-	go StartSearch()
-	go StartSearch()
-	go StartSearch()
+	searchQueue = make(chan queryAnswer, 200)
+	for i := 0; i < 4; i++ {
+		go StartSearch()
+	}
 	loadData()
 	startHttp()
 }
